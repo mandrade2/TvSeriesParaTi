64e368c8b27fda2152c0b43b089824bf487b0221
Juan Castro <jecastro2@uc.cl>
2017-04-26 11:14:51 -0300

    arreglado bug devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :role) }
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name, :role)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name role])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
-  get 'pages/news'
+  get 'news', to: 'pages#news'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
