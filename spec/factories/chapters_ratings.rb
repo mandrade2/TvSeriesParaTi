c4a5566c404d952e83a36c22c528f01275af70bc
arturodelreal <aodelreal@uc.cl>
2017-05-19 13:16:32 -0400

    borrar spec

--- spec/factories/chapters_ratings.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :chapters_rating do
-    rating 1
-    user nil
-    chapter nil
-  end
-end
--- spec/factories/series_ratings.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :series_rating do
-    rating 1
-    user nil
-    series nil
-  end
-end
