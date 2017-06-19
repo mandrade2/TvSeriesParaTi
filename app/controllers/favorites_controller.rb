dc5d631585904c462e30d7218efa08d2d1b1748a
Juan Castro <jecastro2@uc.cl>
2017-06-18 21:24:02 -0400

    terminado favoritos de series y capitulos

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,6 @@
 class FavoritesController < ApplicationController
+  before_action :set_favorites, only: %i[create destroy]
+
   def index
     @user = current_user
     @series = current_user.favorite_series
@@ -6,8 +8,25 @@ class FavoritesController < ApplicationController
   end
 
   def create
+    current_user.favorite_series << @series unless @series.nil?
+    current_user.favorite_chapters << @chapter unless @chapter.nil?
+    redirect_to @series unless @series.nil?
+    unless @chapter.nil?
+      redirect_to series_chapter_path(@chapter.season.series, @chapter)
+    end
   end
 
   def destroy
+    current_user.favorite_series.delete(@series) unless @series.nil?
+    current_user.favorite_chapters.delete(@chapter) unless @chapter.nil?
+    redirect_to @series unless @series.nil?
+    unless @chapter.nil?
+      redirect_to series_chapter_path(@chapter.season.series, @chapter)
+    end
+  end
+
+  def set_favorites
+    @series = Series.find(params[:series_id]) if params[:series_id]
+    @chapter = Chapter.find(params[:chapter_id]) if params[:chapter_id]
   end
 end
