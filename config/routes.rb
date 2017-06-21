a5e8c9d8107fad073dfd680c07362209a8394520
Matias Andrade <mandrade2@uc.cl>
2017-06-20 23:10:57 -0400

    fix fb

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,10 @@ Rails.application.routes.draw do
   resources :favorites, only: %i[index create destroy]
 
   devise_for :users , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :users, only: %i[index] do
     collection do
       get 'search'
