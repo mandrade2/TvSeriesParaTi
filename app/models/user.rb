9bafda361bf6cfb4ba9ddfb38c0e74d5aaf777fb
Matias Andrade <mandrade2@uc.cl>
2017-06-20 22:51:38 -0400

    fix fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -108,12 +108,12 @@ class User < ApplicationRecord
       user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.skip_confirmation!
     end
   end
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if data == session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
