3eb085a0c56c86009cfa1859199fa8369a20d6bb
Matias Andrade <mandrade2@uc.cl>
2017-06-20 23:18:45 -0400

    fix fb

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,9 @@ Rails.application.routes.draw do
   end
 
   resources :favorites, only: %i[index create destroy]
-
+  get 'users/auth/facebook/callback', to: 'series#index'
   devise_for :users , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+
   resources :users, only: %i[index] do
     collection do
       get 'search'
