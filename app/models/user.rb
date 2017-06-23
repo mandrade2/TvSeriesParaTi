800d2427ed73aa867aa0bf64aaccb51b329b5f4b
Matias Andrade <mandrade2@uc.cl>
2017-06-23 18:38:31 -0400

    fix fb

--- app/models/user.rb
+++ app/models/user.rb
@@ -101,12 +101,14 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
+    debug auth.inspect
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
+      debug user.inspect
       user.skip_confirmation!
     end
   end
