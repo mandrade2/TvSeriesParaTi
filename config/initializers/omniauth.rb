1a46e118ab5e7b1b88a2f59d80acfb8a2dc2dca0
Matias Andrade <mandrade2@uc.cl>
2017-06-23 18:22:06 -0400

    fix fb

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook,'437146823325434', '608e603a6a75a906f071b4c544b7abc4'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,8 @@ Rails.application.routes.draw do
 
   resources :favorites, only: %i[index create destroy]
   devise_for :users , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  get 'users/auth/facebook/callback', to: 'series#index'
-  get '/auth/:facebook/callback', to: 'sessions#create'
+  #get 'users/auth/facebook/callback', to: 'series#index'
+  #get '/auth/:facebook/callback', to: 'sessions#create'
 
   resources :users, only: %i[index] do
     collection do
