f2be26f4a8c3a1b5725813280fd598cec0b0c3a5
Matias Andrade <mandrade2@uc.cl>
2017-06-23 21:21:59 -0400

    controladores devise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
@@ -14,7 +17,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
+  # GET|POST /users/auth/twitter/callback
   def failure
     redirect_to root_path
   end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   resources :favorites, only: %i[index create destroy]
-  devise_for :users , :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users , controllers: { sessions: "users/sessions", omniauth_callbacks: "users/omniauth_callbacks" }
   #get 'users/auth/facebook/callback', to: 'series#index'
   #get '/auth/:facebook/callback', to: 'sessions#create'
 
