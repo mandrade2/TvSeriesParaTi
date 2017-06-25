309ee989dc45fba9573365c0e515153262d54bbd
Matias Andrade <mandrade2@uc.cl>
2017-06-25 12:27:53 -0400

    facebook api

--- app/models/user.rb
+++ app/models/user.rb
@@ -112,7 +112,7 @@ class User < ApplicationRecord
   end
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data == session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if params == session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
